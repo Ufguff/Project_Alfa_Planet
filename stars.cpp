6cba2375bf30a0bc1338dbe09082629099a0bc4b
Vladimir-Shat <131755010+Vladimir-Shat@users.noreply.github.com>
2023-07-19 10:44:40 +0500

    Update stars.cpp

--- stars.cpp
+++ stars.cpp
@@ -1,47 +1,36 @@
-#include <iostream>
 #include <ctime>
-#include <unistd.h>
+#define WX 1200
+#define WY 700
+#define col_stars 150
+#include "stars.h"
 #include "graphics.h"
 
-using namespace std;
-
-//const int wx = 1200, wy = 700; // размеры окна
-
-const int col_stars = 150; // общее количество звёзд
-int colors[10] = { COLOR(252, 245, 245), COLOR(127, 239, 153), COLOR(239, 153, 127), COLOR(214, 210, 239), COLOR(239, 191, 127), COLOR(242, 176, 237) };
-
-typedef struct remember { // структура хранения координат звёзд
-   int ir, jr;
-} remember;
+int colors[10] = { COLOR(252, 245, 245), COLOR(127, 239, 153), COLOR(239, 153, 127), COLOR(214, 210, 239), COLOR(239, 191, 127), COLOR(242, 176, 237) }; // РјР°СЃСЃРёРІ С†РІРµС‚РѕРІ Р·РІС‘Р·Рґ
 remember stars_rem[col_stars];
 
-void rast_stars(int i) { // расстановка звёзд
-   stars_rem[i].ir = rand() % wx;
-   stars_rem[i].jr = rand() % wy;
-   setfillstyle(SOLID_FILL, colors[rand() % 7]); // отрисовка звезды со случайным цветом из набора цветов
-   bar(stars_rem[i].ir, stars_rem[i].jr, stars_rem[i].ir+1, stars_rem[i].jr+1);
+void scol_bar(int col, int col_help) { // СѓСЃС‚Р°РЅРѕРІРєР° С†РІРµС‚Р° Рё РѕС‚СЂРёСЃРѕРІРєР° Р·РІС‘Р·Рґ
+   setfillstyle(SOLID_FILL, col);
+   bar(stars_rem[col_help].ir, stars_rem[col_help].jr, stars_rem[col_help].ir+1, stars_rem[col_help].jr+1);
 }
 
-void stars() { // изначальная случайная расстановка звёзд
+void rast_stars(int i) { // СЂР°СЃСЃС‚Р°РЅРѕРІРєР° Р·РІС‘Р·Рґ
+   stars_rem[i].ir = rand() % WX;
+   stars_rem[i].jr = rand() % WY;
+   stars_rem[i].col = rand() % 7;
+   scol_bar(colors[stars_rem[i].col], i);
+}
+
+void stars() { // РёР·РЅР°С‡Р°Р»СЊРЅР°СЏ СЃР»СѓС‡Р°Р№РЅР°СЏ СЂР°СЃСЃС‚Р°РЅРѕРІРєР° Р·РІС‘Р·Рґ
    srand(static_cast<unsigned>(time(NULL)));
    for (int i = 0; i < col_stars; i++)
       rast_stars(i);
 }
 
-void stars_for_main() { // закрышивание случайных звёзд чёрным и отрисовка новых на смену закрашенным
+void stars_for_main() { // Р·Р°РєСЂС‹С€РёРІР°РЅРёРµ СЃР»СѓС‡Р°Р№РЅС‹С… Р·РІС‘Р·Рґ С‡С‘СЂРЅС‹Рј Рё РѕС‚СЂРёСЃРѕРІРєР° РЅРѕРІС‹С… РЅР° СЃРјРµРЅСѓ Р·Р°РєСЂР°С€РµРЅРЅС‹Рј
    int col_help = rand() % col_stars;
-   setfillstyle(SOLID_FILL, BLACK);
-   bar(stars_rem[col_help].ir, stars_rem[col_help].jr, stars_rem[col_help].ir+1, stars_rem[col_help].jr+1);
+   scol_bar(BLACK, col_help);
    rast_stars(col_help);
-   delay(3);
+   clearviewport();
+   for (int j = 0; j < col_stars; j++)
+      scol_bar(colors[stars_rem[j].col], j);
 }
-
-/*
-int main() {
-   initwindow(wx, wy, "Звёзды", 0, 0);
-   stars();
-   while (1)
-      stars_for_main();
-   
-   return 0;
-}*/
