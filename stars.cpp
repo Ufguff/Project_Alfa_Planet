935a630b71bffcc8f6b9077216e4d98e0197b71d
Vladimir-Shat <131755010+Vladimir-Shat@users.noreply.github.com>
2023-07-19 21:38:46 +0500

    Update stars.cpp

--- stars.cpp
+++ stars.cpp
@@ -5,19 +5,15 @@
 #include "stars.h"
 #include "graphics.h"
 
+int count_st = 0;
 int colors[10] = { COLOR(252, 245, 245), COLOR(127, 239, 153), COLOR(239, 153, 127), COLOR(214, 210, 239), COLOR(239, 191, 127), COLOR(242, 176, 237) }; // массив цветов звёзд
 remember stars_rem[col_stars];
 
-void scol_bar(int col, int col_help) { // установка цвета и отрисовка звёзд
-   setfillstyle(SOLID_FILL, col);
-   bar(stars_rem[col_help].ir, stars_rem[col_help].jr, stars_rem[col_help].ir+1, stars_rem[col_help].jr+1);
-}
-
-void rast_stars(int i) { // расстановка звёзд
-   stars_rem[i].ir = rand() % WX;
-   stars_rem[i].jr = rand() % WY;
+void rast_stars(int i) { // случайный выбор расположений и цветов звёзд
+   stars_rem[i].x = rand() % WX;
+   stars_rem[i].y = rand() % WY;
    stars_rem[i].col = rand() % 7;
-   scol_bar(colors[stars_rem[i].col], i);
+   putpixel(stars_rem[i].x, stars_rem[i].y, colors[stars_rem[i].col]);
 }
 
 void stars() { // изначальная случайная расстановка звёзд
@@ -26,11 +22,14 @@ void stars() { // изначальная случайная расстановк
       rast_stars(i);
 }
 
-void stars_for_main() { // закрышивание случайных звёзд чёрным и отрисовка новых на смену закрашенным
-   int col_help = rand() % col_stars;
-   scol_bar(BLACK, col_help);
-   rast_stars(col_help);
+void stars_for_main() { // закрашивание случайных звёзд и отрисовка новых на смену закрашенным
+   if (count_st == 45) {
+      int col_help = rand() % col_stars;
+      rast_stars(col_help);
+      count_st = 0;
+   }
+   count_st++;
    clearviewport();
    for (int j = 0; j < col_stars; j++)
-      scol_bar(colors[stars_rem[j].col], j);
+      putpixel(stars_rem[j].x, stars_rem[j].y, colors[stars_rem[j].col]);
 }
